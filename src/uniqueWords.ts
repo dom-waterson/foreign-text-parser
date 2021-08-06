bc2990f99ea8fb24b83f051ad2b3c20e7f649863
dom-waterson <dom_waterson@hotmail.com>
2021-08-06 18:42:09 +0100

    feat: added options

--- src/uniqueWords.ts
+++ src/uniqueWords.ts
@@ -2,7 +2,12 @@ interface WordFrequencies {
   [name: string]: number;
 }
 
-export const uniqueWords = (text: string, wordLimit?: number): Promise<string[]> => {
+interface OptionParamaters {
+  wordLimit?: number;
+  ignoreWords?: string[];
+}
+
+export const uniqueWords = (text: string, options?: OptionParamaters): Promise<string[]> => {
   return new Promise((resolve) => {
     const sanitizedText = text.replace(/[.,-/#!$%^&*;:{}=\-_`~()\n]/g, '');
 
@@ -20,8 +25,16 @@ export const uniqueWords = (text: string, wordLimit?: number): Promise<string[]>
       return wordFrequencies;
     }, {});
 
-    const sortedWords = Object.keys(words).sort((a, b) => words[b] - words[a]);
+    let sortedWords = Object.keys(words).sort((a, b) => words[b] - words[a]);
 
-    resolve(wordLimit ? sortedWords.slice(0, wordLimit) : sortedWords);
+    if (options?.ignoreWords) {
+      sortedWords = sortedWords.filter((word) => !options.ignoreWords?.includes(word));
+    }
+
+    if (options?.wordLimit) {
+      sortedWords = sortedWords.slice(0, options.wordLimit);
+    }
+
+    resolve(sortedWords);
   });
 };
